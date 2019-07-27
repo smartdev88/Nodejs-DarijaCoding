0ed6c76613486a67ee6be9f435e95caff822f797
yassine <lagyassine88@gmail.com>
2019-07-27 18:19:38 +0000

    v11

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer( (req, res) => {
-    fs.readFile('index.html', 'UTF-8', (err, data) =>
-    {
-        res.writeHead(200, {'content-Type':'text/html'});
-        res.write(data);
-        res.end();
-    })
-}).listen(3000);
+    if(req.url === '/'){
+        fs.readFile('index.html', 'UTF-8', (err, data) =>
+            {
+                res.writeHead(200, {'content-Type':'text/html'});
+                res.write(data);
+                res.end();
+            });
+    } else if (req.url === '/about'){
+        fs.readFile('about.html', 'UTF-8', (err, data) =>
+            {
+                res.writeHead(200, {'content-Type':'text/html'});
+                res.write(data);
+                res.end();
+            });
+    }
+}).listen(2000);
 
-console.log('Server started at port 3000');
+console.log('Server started at port 2000');
