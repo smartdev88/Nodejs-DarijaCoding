78cab66553ad82177d3f4b50c6edfcbc090a9e86
yassine <lagyassine88@gmail.com>
2019-07-27 18:03:29 +0000

    v10

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-//Lecture de fichier data.txt
-/*
+
 var http = require('http');
 var fs = require('fs');
 
-http.createServer( function(req, res) {
-    fs.readFile('data.txt', function(err, data){
-        if(err) throw err;
+http.createServer( (req, res) => {
+    fs.readFile('index.html', 'UTF-8', (err, data) =>
+    {
         res.writeHead(200, {'content-Type':'text/html'});
         res.write(data);
         res.end();
-    });
+    })
 }).listen(3000);
 
-
-console.log("server started at port 3000");
-*/
-
-//ajouter dans le fichier
-
-var http = require('http');
-var fs = require('fs');
-
-http.createServer( function(req, res) {
-    fs.appendFile('data.txt', 'ca va ', function(err){
-        if(err) throw err;
-        console.log('Saved');
-    });
-}).listen(3000);
-
-
-console.log("server started at port 3000");
+console.log('Server started at port 3000');
