05924fe27468c5b7833ca4f66a33c61b257dc52b
yassine <lagyassine88@gmail.com>
2019-07-28 14:59:53 +0000

    v22 les pages get & send

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 
 var app = express();
 
+
+//page index
+app.get('/',function(req,res){
+  res.send('Hello world');
+});
+
+//contact page
+app.get('/contact',function(req,res){
+  res.send('Contact page');
+});
+
+//create server
 app.listen(3000, function () {
-    console.log('server started at port 3000')
-  });
+  console.log('server started at port 3000')
+});
