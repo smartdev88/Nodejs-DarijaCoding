75193de44d6fb068ba8fff0473a4db8b501bdfb3
yassine <lagyassine88@gmail.com>
2019-07-28 11:38:43 +0000

    v17 creation de table

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ var mysql = require('mysql');
 var connexion = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    //ajouté la nom de la base lorsqu'on veut créer la table.
+    database: "appnodejs"
 });
    
 connexion.connect(function(err) {
     if (err) throw err;
     console.log("Connecté");
+//création base donnée
+    /*connexion.query("CREATE DATABASE appNodejs", (err) => {
+        if(err) throw err;
+        console.log("base de donnée ajoutée");
+    });*/
+//creation de la table users
+    var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), passe VARCHAR(255))";
+    connexion.query(sql, (err) => {
+        if(err) throw err;
+        console.log("table ajoutée");
+    });
 });
