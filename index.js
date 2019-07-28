b6ac60b74fa4a8ebcd3d5bfb4aaaaf4f594529f2
yassine <lagyassine88@gmail.com>
2019-07-28 10:33:54 +0000

    v16 creation base donnee

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 
-var http = require('http');
-var fs = require('fs');
+var mysql = require('mysql');
 
-http.createServer( (req, res) => {
-    let formContent = '';
-    if(req.method === 'GET'){
-        fs.readFile('index.html', 'UTF-8', (err, data) =>
-            {
-                if(err) throw err;
-                res.writeHead(200, {'content-Type':'text/html'});
-                res.write(data);
-                res.end();
-            });
-    } else if (req.method === 'POST'){
-                req.on('data', data => {
-                    formContent += data;
-                });
-                req.on('end', () => {
-                    res.writeHead(200, {'content-Type':'text/html'});
-                    res.write(formContent, () => {
-                        res.end();
-                    });
-                });
-        }
-}).listen(3000);
-
-console.log('Server started at port 3000');
+// etablir la connexion
+var connexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+   
+connexion.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté");
+});
