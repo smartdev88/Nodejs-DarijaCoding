e4ccb67833e2ac2e21049be1d236c6a10909a03b
yassine <lagyassine88@gmail.com>
2019-07-28 12:44:25 +0000

    v20 drop table

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var connexion = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    //ajouté la nom de la base lorsqu'on veut créer la table.
     database: "appnodejs"
 });
    
@@ -14,23 +13,12 @@ connexion.connect(function(err) {
     if (err) throw err;
     console.log("Connecté");
 
-    //les requetes sql
-
-    var sql = "UPDATE users SET email = 'achraf', passe = 'achraf' WHERE id = 5";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("modifié");
-    });
-
-    var sql = "SELECT * FROM users LIMIT 3";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("résultat: ", data);
-    });
-
-    var sql = "DELETE FROM users WHERE id = 6";
-    connexion.query(sql, (err, data) => {
+    //suppression de la table users
+    var sql = "DROP TABLE IF EXISTS users";
+    connexion.query(sql, (err) => {
         if(err) throw err;
         console.log("supprimé");
     });
+
+    /*npm install nodemon */
 });
