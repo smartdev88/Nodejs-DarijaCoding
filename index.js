9950c7fc345d818a4ca0893f4f4c70332895ccc2
yassine <lagyassine88@gmail.com>
2019-07-28 12:21:16 +0000

    v19 securite

--- index.js
+++ index.js
@@ -14,36 +14,9 @@ connexion.connect(function(err) {
     if (err) throw err;
     console.log("Connecté");
 
-    //afficher tous les utilisateurs
-    var sql = "SELECT * FROM users";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("utilisateurs : ", data);
-    });
-
-    //afficher un utilisateur avec un id donnée
-    var sql = "SELECT * FROM users WHERE id=4";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("utilisateur est  : ", data);
-    });
-
-    //afficher un utilisateur avec un email donnée
-    var sql = "SELECT * FROM users WHERE email='hamza'";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("Résultat est  : ", data);
-    });
-
-    //afficher  utilisateurs qui email fini par e
-    var sql = "SELECT * FROM users WHERE email LIKE '%e'";
-    connexion.query(sql, (err, data) => {
-        if(err) throw err;
-        console.log("Résultat  : ", data);
-    });
-
-    //afficher  utilisateurs qui email commence par h
-    var sql = "SELECT * FROM users WHERE email LIKE 'h%'";
+    //securité
+    var email = "hamza"
+    var sql = "SELECT * FROM users WHERE email = "+ mysql.escape(email);
     connexion.query(sql, (err, data) => {
         if(err) throw err;
         console.log("Résultat  : ", data);
